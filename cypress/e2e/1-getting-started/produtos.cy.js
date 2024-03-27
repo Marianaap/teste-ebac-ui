0b1416df1acbcbd0be2cfdfd37832aa020a3d5bd
Mariana Silva <marianasilvaap1@gmail.com>
2024-03-27 15:39:10 -0300

    Alterações nos teste automatizados

---
+++ cypress/e2e/1-getting-started/produtos.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade Página de produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click()
+
+    });
+
+    it.only('Deve adicionar um produto ao carrinho', () => {
+        cy.get('[class="product-block grid"]')
+            .contains('Ariel Roll Sleeve Sweatshirt').click()
+        cy.get('.button-variable-item-S').click()
+        cy.get('.button-variable-item-Purple').click()
+        cy.get('.input-text').clear().type(2)
+        cy.get('.single_add_to_cart_button').click()
+
+
+    })
+
+});
